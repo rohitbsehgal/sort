0c13c518acefa3b3d400891f0de47f1ae471bd08
Rohit Sehgal <rohitben@hotmail.com>
2015-06-08 13:28:48 -0500

    Update sort.js

--- sort.js
+++ sort.js
@@ -1,3 +1,16 @@
 function sort(paragraph){
-
+var array = paragraph.split(" ");
+for(var k = 0; k < array.length; k++){
+	array[k] = [array[k], 1];
+}
+console.log(array);
+for(var i = 0; i < array.length; i++){
+  for(var j = 1; j < array.length; j++){
+    if(array[i] == array[j]){
+    	array.splice(j-1, 1);
+    	array[i][1] += 1;
+    }
+  }
+}
+console.log(array);
 }
