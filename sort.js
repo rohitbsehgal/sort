a5e740aaf242c8143e5ff93960f93bb7b0d884ce
Rohit Sehgal <rohitben@hotmail.com>
2015-06-11 13:05:00 -0500

    Update sort.js

--- sort.js
+++ sort.js
@@ -3,14 +3,21 @@ var array = paragraph.split(" ");
 for(var k = 0; k < array.length; k++){
 	array[k] = [array[k], 1];
 }
-console.log(array);
 for(var i = 0; i < array.length; i++){
-  for(var j = 1; j < array.length; j++){
-    if(array[i] == array[j]){
-    	array.splice(j-1, 1);
-    	array[i][1] += 1;
+  var x = i+1;
+  if(i === 0){
+    x = 1;
+  }
+  for(var j = x; j < array.length; j++){
+    if(array[i][0] == array[j][0]){
+    	console.log(array);
+        console.log(i + ", " + j);
+        array[i][1] += 1;
+        array.splice(j, 1);
+        console.log(array);
+        j-=1;
     }
   }
 }
-console.log(array);
+return array;
 }
